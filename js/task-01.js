4d63bef67812acf9b75595cc9636a2f527577152
PetroTkach <81279214+PetroTkach@users.noreply.github.com>
2022-01-31 15:37:58 +0200

    task-01 Done

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
-const navEl = document.querySelector("ul");
-console.log(navEl);
+const navItemEl = document.querySelectorAll(".item");
+console.log("Number of categories:", navItemEl.length);
+
+// const navTitleEl1 = document.querySelectorAll("h2");
+// console.log("Category:", navTitleEl1[0].textContent);
+
+navItemEl.forEach((elem) => {
+  console.log("Category:", elem.firstElementChild.textContent);
+  console.log("Elements:", elem.lastElementChild.children.length);
+});
