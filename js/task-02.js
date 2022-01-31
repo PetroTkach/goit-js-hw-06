87e37ba5487ef513b8a2b75d9ea3cb2449d3b8a4
PetroTkach <81279214+PetroTkach@users.noreply.github.com>
2022-01-31 16:45:37 +0200

    task-02 Done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const navEl = document.querySelector("ul#ingredients");
+console.log(navEl);
+
+// const navItem = document.createElement("li");
+ingredients.forEach((element) => {
+  const navItemEl = document.createElement("li");
+  navItemEl.classList.add("item");
+  navItemEl.textContent = element;
+  console.log(navItemEl);
+  navEl.append(navItemEl)
+});
