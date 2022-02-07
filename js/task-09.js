a18c7238053faf05265a06a388e233c6d3071ee1
PetroTkach <petro.tka4444@gmail.com>
2022-02-07 20:24:46 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const bodyEl = document.querySelector("body");
+// console.log("bodyEl", bodyEl);
+const btnEl = document.querySelector(".change-color");
+// console.log("btnEl", btnEl);
+const spanEl = document.querySelector(".color");
+// console.log("spanEl", spanEl);
+
+btnEl.addEventListener("click", onBodyColor);
+
+function onBodyColor(elem) {
+  const newColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = newColor;
+  spanEl.textContent = newColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
